12038ff476a6840d4bd3b005676369c09985d6d1
Marta <Marta@DESKTOP-U7E99P0>
2019-05-04 18:07:22 +0200

    última versión

--- DomHogar-master/DomHogar-master/src/controlador/Eventos.java
+++ DomHogar-master/DomHogar-master/src/controlador/Eventos.java
@@ -151,6 +151,7 @@ public class Eventos implements ActionListener, MouseListener {
 		
 		else if (e.getSource()==ventana.getBotonPurchases()) {
 			ventana.getBotonPurchases().setIcon(new ImageIcon("img/purchases hover.png"));
+			
 		}
 		
 		else if (e.getSource()==ventana.getBotonSales()) {
@@ -243,47 +244,70 @@ public class Eventos implements ActionListener, MouseListener {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(e.getSource()==ventana.getBotonLogin()) {
-			 ventana.getBotonLogin().setBackground(Color.WHITE);
-			 ventana.getBotonLogin().setIcon(new ImageIcon("imagenes/login press.png"));			 
+		if(e.getSource()==ventana.getBotonLogin()) {		 
+			 ventana.getBotonLogin().setIcon(new ImageIcon("imagenes/login press.png"));
+			 ventana.getBotonLogin().setContentAreaFilled(false);
+			 
+			 
 		}
 		else if (e.getSource()==ventana.getBotonExit()) {
-			 
 			ventana.getBotonExit().setIcon(new ImageIcon("imagenes/exit login press.png"));
-			ventana.getBotonLogin().setBackground(Color.WHITE);
-		}		
+			ventana.getBotonExit().setContentAreaFilled(false); 
+			
+		}
+		
 		else if (e.getSource()==ventana.getBotonPurchases()) {
 			ventana.getBotonPurchases().setIcon(new ImageIcon("img/purchases press.png"));
-			ventana.getBotonLogin().setBackground(Color.WHITE);
-		}		
+			ventana.getBotonPurchases().setContentAreaFilled(false);
+			
+			
+		}
+		
 		else if (e.getSource()==ventana.getBotonSales()) {
 			ventana.getBotonSales().setIcon(new ImageIcon("img/sales press.png"));
-		}		
+			ventana.getBotonSales().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonSuppliers()) {
 			ventana.getBotonSuppliers().setIcon(new ImageIcon("img/suppliers press.png"));
-		}		
+			ventana.getBotonSuppliers().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonCustomers()) {
 			ventana.getBotonCustomers().setIcon(new ImageIcon("img/customers press.png"));
-		}		
+			ventana.getBotonCustomers().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonServices()) {
 			ventana.getBotonServices().setIcon(new ImageIcon("img/services press.png"));
-		}		
+			ventana.getBotonServices().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonStock()) {
 			ventana.getBotonStock().setIcon(new ImageIcon("img/stock press.png"));
-		}		
+			ventana.getBotonStock().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonHR()) {
 			ventana.getBotonHR().setIcon(new ImageIcon("img/human resources press.png"));
+			ventana.getBotonHR().setContentAreaFilled(false);
 		}
 		
 		else if (e.getSource()==ventana.getBotonUser()) {
 			ventana.getBotonUser().setIcon(new ImageIcon("img/boton_user press.png"));
-		}		
+			ventana.getBotonUser().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonLogout()) {
 			ventana.getBotonLogout().setIcon(new ImageIcon("img/logout press.png"));
-		}		
+			ventana.getBotonLogout().setContentAreaFilled(false);
+		}
+		
 		else if (e.getSource()==ventana.getBotonExitInit()) {
 			ventana.getBotonExitInit().setIcon(new ImageIcon("img/exit press.png"));
-		}		
+			ventana.getBotonExitInit().setContentAreaFilled(false);
+		}
+		
 	}
 
 	@Override
